508cbdadb50b24ac30f31258ad45264134c3177b
Quinn Reynolds <quinnr@mintek.co.za>
2019-08-08 13:26:34 +0200

    Overhaul of furnace model to allow interface deformation near taphole

--- ladlesim/femn.py
+++ ladlesim/femn.py
@@ -26,7 +26,8 @@ class FeMnFurnace():
     
     def calc_vdot_out(self):
         """Return outlet flowrates as a function of the current furnace state. 
-        Simplified model without interface deformation near tap-hole entry.
+        Extended model with semi-empirical interface deformation near tap-hole 
+        entry.
         """
         rt = 0.5 * self.tapholediameter
         eff_d = self.particlesphericity * self.particlediameter
@@ -40,61 +41,86 @@ class FeMnFurnace():
                (3 * eff_d * self.bedporosity**3))
         b_s = (1.75 * self.densityslag * rt * (1-self.bedporosity) / 
                (3 * eff_d * self.bedporosity**3))
+        
+        # tapping pressure calculation (applied to both phases, based on 
+        # undeformed interfaces). Integrated pressure field in z, averaged with 
+        # total height in taphole.
         if self.hslag < -rt:
             # slag below taphole - no flow
-            pa_m = 0
-            pa_s = 0
+            pa = 0
+        elif self.hslag < rt:
+            #taphole partially filled
+            if self.hmetal < -rt:
+                # slag only
+                pa = 0.5 * (self.hslag + rt) * self.densityslag * g
+            else:
+                # slag and metal
+                pa = 0.5*g*(self.densitymetal*(self.hmetal+rt)**2 + 
+                            self.densityslag*(self.hslag*(self.hslag+2*rt) - 
+                            self.hmetal*(self.hmetal+2*rt))) / (self.hslag+rt)
+        else:
+            # taphole completely filled
+            if self.hmetal < -rt:
+                # slag only
+                pa = self.hslag * self.densityslag * g
+            elif self.hmetal < rt:
+                # slag and metal
+                pa = 0.25*g*(self.densitymetal*(self.hmetal+rt)**2 + 
+                             self.densityslag*(rt*(4*self.hslag-rt) - 
+                                        self.hmetal*(self.hmetal+2*rt))) / rt
+            else:
+                # metal only
+                pa = g * ((self.hslag-self.hmetal) * self.densityslag + 
+                            self.hmetal * self.densitymetal)
+        
+        # estimate single-phase-only velocities
+        umetal = (-b_m+np.sqrt(b_m**2 + 4*pa*a_m)) / (2*a_m)
+        uslag = (-b_s+np.sqrt(b_s**2 + 4*pa*a_s)) / (2*a_s)
+        h0_l = -rt - self.densityslag*uslag**2 / (8*g*(self.densitymetal - 
+                                                       self.densityslag))
+        h0_h = rt + self.densitymetal*umetal**2 / (8*g*(self.densitymetal - 
+                                                        self.densityslag))
+        
+        if self.hslag < -rt:
+            # slag below taphole - no flow
             area_m = 0
             area_s = 0
         elif self.hslag < rt:
-            if self.hmetal < -rt:
-                # only slag, partially filled taphole
-                pa_m = 0
-                pa_s = 0.5 * (self.hslag + rt) * self.densityslag * g
-                theta = 2*np.arccos(1 - (self.hslag + rt)/rt)
+            #taphole partially filled
+            if self.hmetal < h0_l:
+                # slag only
+                theta = 2*np.arccos(-self.hslag/rt)
                 area_m = 0
-                area_s = 0.5*rt**2 * (theta - np.sin(theta)) 
+                area_s = 0.5*rt**2*(theta - np.sin(theta))
             else:
-                # slag and metal, partially filled taphpole
-                pa_m = 0.5 * g * (2*(self.hslag-self.hmetal)*self.densityslag +
-                                  (self.hmetal+rt)*self.densitymetal)
-                pa_s = 0.5 * (self.hslag-self.hmetal) * self.densityslag * g
-                theta = 2*np.arccos(1 - (self.hslag + rt)/rt)
-                area_tot = 0.5*rt**2 * (theta - np.sin(theta)) 
-                theta = 2*np.arccos(1 - (self.hmetal + rt)/rt)                
-                area_m = 0.5*rt**2 * (theta - np.sin(theta)) 
-                area_s = area_tot - area_m
+                # slag and metal
+                hi = self.hslag - ((self.hslag-self.hmetal) * (self.hslag+rt) / 
+                                   (self.hslag-h0_l))
+                theta = 2*np.arccos(-hi/rt)
+                area_m = 0.5*rt**2*(theta - np.sin(theta))
+                theta = 2*np.arccos(-self.hslag/rt)
+                area_s = 0.5*rt**2*(theta - np.sin(theta)) - area_m
         else:
-            if self.hmetal < -rt:
-                # only slag, filled taphole
-                pa_m = 0
-                pa_s = self.hslag * self.densityslag * g
+            # taphole completely filled
+            if self.hmetal < h0_l:
+                # slag only
                 area_m = 0
-                area_s = 2*np.pi*rt**2
-            elif self.hmetal < rt:
-                # slag and metal, filled taphole
-                pa_m = 0.5 * g * (2*(self.hslag-self.hmetal)*self.densityslag +
-                                  (self.hmetal+rt)*self.densitymetal)
-                pa_s = 0.5 * g * ((self.hslag-self.hmetal) * self.densityslag + 
-                                  (self.hslag-rt) * self.densityslag)
-                theta = 2*np.arccos(1 - (self.hmetal + rt)/rt)
-                area_m = 0.5*rt**2 * (theta - np.sin(theta)) 
-                area_s = np.pi*rt**2 - area_m
-            else:
-                # metal only, filled taphole
-                pa_m = g * ((self.hslag-self.hmetal) * self.densityslag + 
-                            self.hmetal * self.densitymetal)
-                pa_s = 0
+                area_s = np.pi*rt**2
+            elif self.hmetal > h0_h:
+                # metal only
                 area_m = np.pi*rt**2
                 area_s = 0
-        
+            else:
+                # slag and metal
+                hi = rt*(1 - 2*(h0_h-self.hmetal)/(h0_h-h0_l))
+                theta = 2*np.arccos(-hi/rt)
+                area_m = 0.5*rt**2*(theta - np.sin(theta))
+                area_s = np.pi*rt**2 - area_m
+                
         if not self.tapholeopen_yn:
-            pa_m = 0
-            pa_s = 0
             area_m = 0
             area_s = 0
-        umetal = (-b_m+np.sqrt(b_m**2 + 4*pa_m*a_m)) / (2*a_m)
-        uslag = (-b_s+np.sqrt(b_s**2 + 4*pa_s*a_s)) / (2*a_s)
+
         vdot_metal = area_m * umetal
         vdot_slag = area_s * uslag
         self.vdotmetal, self.vdotslag = vdot_metal, vdot_slag
