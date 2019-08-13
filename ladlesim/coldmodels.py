6bc0ea321d6188bb1a7ce7df5fa03c33728d2d0c
Quinn Reynolds <quinnr@mintek.co.za>
2019-08-13 13:04:04 +0200

    Fix Vango case implementation and specifications

--- ladlesim/coldmodels.py
+++ ladlesim/coldmodels.py
@@ -6,12 +6,11 @@ import numpy as np
 from scipy.constants import g, pi
 
 class TankWithPorousBed():
-    def __init__(self, tankarea, tapholewidth, tapholeheight, kl, densityfluid, 
+    def __init__(self, tankarea, tapholediameter, kl, densityfluid, 
                  viscosityfluid, particlediameter, particlesphericity, 
                  bedporosity, bedheight, hfluid_init):
         self.tankarea = tankarea
-        self.tapholeheight = tapholeheight
-        self.tapholewidth = tapholewidth
+        self.tapholediameter = tapholediameter
         self.kl = kl
         self.densityfluid = densityfluid
         self.viscosityfluid = viscosityfluid
@@ -25,20 +24,21 @@ class TankWithPorousBed():
         """Return outlet flowrates as a function of the current tank state. 
         Simplified model without interface deformation near tap-hole entry.
         """
-        rt = np.sqrt(self.tapholewidth * self.tapholeheight / pi)
+        rt = 0.5 * self.tapholediameter
         eff_d = self.particlesphericity * self.particlediameter
         a = (150 * self.viscosityfluid * rt * (1-self.bedporosity)**2 / 
              (eff_d**2 * self.bedporosity**3) +
              0.5*(1+self.kl)*self.densityfluid)
         b = (1.75 * self.densityfluid * rt * (1-self.bedporosity) / 
              (3 * eff_d * self.bedporosity**3))
-        if self.hfluid < self.tapholeheight:
+        if self.hfluid < self.tapholediameter:
             # partially filled taphole
             pa = 0.5 * self.hfluid * self.densityfluid * g
-            xarea = self.hfluid * self.tapholewidth 
+            theta = 2*np.arccos(1-self.hfluid/rt)
+            xarea = 0.5 * rt**2 * (theta - np.sin(theta))
         else:
-            pa = (self.hfluid - 0.5*rt) * self.densityfluid * g
-            xarea = self.tapholeheight * self.tapholewidth
+            pa = (self.hfluid - rt) * self.densityfluid * g
+            xarea = pi * rt**2
         ufluid = (-b + np.sqrt(b**2 + 4*pa*a)) / (2*a)
         vdot = xarea * ufluid
         self.vdot = vdot
@@ -54,11 +54,10 @@ class TankWithPorousBed():
         return vdf
 
 def vango_parameters():
-    return {'tankarea': 0.33*0.15, 'tapholewidth': 0.031, 
-            'tapholeheight': 0.0155, 'kl': 0.5, 'densityfluid': 1000, 
-            'viscosityfluid': 0.001, 'particlediameter': 0.0065, 
-            'particlesphericity': 0.75, 'bedporosity': 0.52, 'bedheight': 0.25,
-            'hfluid_init': 0.3}
+    return {'tankarea': 0.33*0.15, 'tapholediameter': 0.031, 'kl': 0.5, 
+            'densityfluid': 1000, 'viscosityfluid': 0.001, 
+            'particlediameter': 0.0065, 'particlesphericity': 0.75, 
+            'bedporosity': 0.52, 'bedheight': 0.25, 'hfluid_init': 0.3}
     
 def vango_experiment_data():
     return {'time': [7.87, 16.2, 27.8, 41.8, 63.8],
