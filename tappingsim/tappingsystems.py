3ef6ec0ba1eca139f74a6933726f3437d018e430
Quinn Reynolds <quinnr@MacBook.local>
2021-08-31 08:34:04 +0200

    Added simple SAF system

--- tappingsim/tappingsystems.py
+++ tappingsim/tappingsystems.py
@@ -61,4 +61,43 @@ class FeMnSAF():
         self.slagmasstotaliser += dt * (self.furnace.vdotslag_out 
                                          * self.furnace.densityslag)
         
-                
+         
+class SimpleSAF():
+    def __init__(self, furnace):
+        self.furnace = furnace
+        self.timetotaliser = 0
+        self.powertotaliserkWh = 0
+        self.metalmasstotaliser = 0
+        self.slagmasstotaliser = 0
+    
+    def open_taphole(self):
+        self.furnace.tapholeopen_yn = True
+        
+    def close_taphole(self):
+        self.furnace.tapholeopen_yn = False
+    
+    def reset_time_totaliser(self):
+        self.timetotaliser = 0
+        
+    def reset_power_totaliser(self):
+        self.powertotaliserkWh = 0
+        
+    def reset_mass_totaliser(self):
+        self.metalmasstotaliser = 0
+        self.slagmasstotaliser = 0
+    
+    def reset_all_totalisers(self):
+        self.reset_time_totaliser()
+        self.reset_power_totaliser()
+        self.reset_mass_totaliser()
+        
+    def calc_dt(self, dt):
+        self.furnace.calc_dt(dt)    
+        self.timetotaliser += dt
+        self.powertotaliserkWh += dt * (POWER_TIME_FACTOR 
+                                        * self.furnace.powerMVA 
+                                        * self.furnace.powerfactor)
+        self.metalmasstotaliser += dt * (self.furnace.vdotmetal_out 
+                                         * self.furnace.densitymetal)
+        self.slagmasstotaliser += dt * (self.furnace.vdotslag_out 
+                                         * self.furnace.densityslag)
